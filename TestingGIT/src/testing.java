3d2a9233f112dd172ae7e2a1197e1ac648f2a77a
xiaoyu-1998 <58579444+xiaoyu-1998@users.noreply.github.com>
2019-12-12 10:03:01 +0800

    Update testing.java

--- TestingGIT/src/testing.java
+++ TestingGIT/src/testing.java
@@ -2,7 +2,7 @@
 public class testing {
 	public void main(String[] args) {
 		System.out.println("Prepare time to dead");
-//		+_+ !!!
+//		+_+ !!! ///
 		for(int i = 0 ; i < 5; i++) {
 			// Testing for update!
 		}
